1f29a1b17c86257c0b79a3fb10905dd55dd5880f
Hirushini Dematagoda <87405106+hirushinidem@users.noreply.github.com>
2023-04-06 05:52:32 +0530

    mongoDB and mongoose added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const orgRoutes = require('./routes/organizations')
 
 const app = express()
@@ -14,7 +15,16 @@ app.use((req, res, next) => {
 
 app.use('/api/organizations', orgRoutes)
 
-app.listen(process.env.PORT, () => {
-  console.log('listening on the port', process.env.PORT)
-})
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db & listening on the port', process.env.PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+
 
